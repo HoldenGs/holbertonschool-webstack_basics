8005afec5e42e497ece458fdd51eb25351e52995
Holden Grissett <holdeng96@gmail.com>
2018-02-15 17:56:12 -0800

    semistandard on 2

--- 0x02-javascript_basics/2-value_argument.js
+++ 0x02-javascript_basics/2-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 let i;
-for (i = 0; process.argv[i]; i++) {};
+for (i = 0; process.argv[i]; i++) {}
 if (i === 3) {
-	console.log(process.argv[i - 1]);
+  console.log(process.argv[i - 1]);
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
