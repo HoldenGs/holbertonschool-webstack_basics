b39e444b918e70b0b44254c456e68a1c797c673a
Holden Grissett <holdeng96@gmail.com>
2018-02-16 14:55:54 -0800

    semistandard

--- 0x02-javascript_basics/100-map.js
+++ 0x02-javascript_basics/100-map.js
@@ -2,6 +2,6 @@
 
 const list = require('./100-data.js').list;
 
-let list2 = list.map(function (x, i) {return x * i});
+let list2 = list.map(function (x, i) { return x * i; });
 console.log(list);
 console.log(list2);
